55dc2b5cb5f85cc49974c63ae070627c05d7a1a0
Kyna2k <huynobi1809@gmail.com>
2023-03-15 15:40:08 +0700

    teo

--- Script/LoginUser.cs
+++ Script/LoginUser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,15 +10,28 @@ public class LoginUser : MonoBehaviour
     // Start is called before the first frame update
     public InputField editUser, editPass;
     public Text thongbao;
+    public Selectable first;
+    private EventSystem eventSystem;
+    public Button btn_login;
     void Start()
     {
-        
+        eventSystem= EventSystem.current;
+        first.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Return))
+        {
+            btn_login.onClick.Invoke();
+        }
+        if(Input.GetKeyDown(KeyCode.Tab)) 
+        { 
+            Selectable next = eventSystem.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            next?.Select();
+
+        }
     }
     public void CheckLogin()
     {
